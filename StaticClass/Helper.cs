4ad6ebe9d943d3a35c0d51826b8293b4125f66d3
Ismayilgit24 <ismayilibrahinzad9@gmail.com>
2024-10-22 22:34:26 +0400

    initial commit

---
+++ StaticClass/Helper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StaticClass
+{
+	internal static class Helper
+	{
+        public static bool IsOdd(this int number)
+		{
+			if(number%2 == 1)
+			{
+				return true;
+			}
+			return false;
+		}
+		public static bool IsEven(this int number)
+		{
+			if (number % 2 == 0)
+			{
+				return true;
+			}
+			return false;
+		
+		}
+
+		public static bool HasDigit(this string word)
+		{
+			for(int i=0; i<word.Length; i++)
+			{
+				if (char.IsDigit(word[i]))
+				{
+					return true;
+				}
+				
+			}
+			return false;
+		}
+
+		public static bool CheckPassword(this string password)
+		{
+			for(int i=0; i<password.Length; i++)
+			{
+				if (password.Length < 8)
+					return false;
+
+				
+				if (!Regex.IsMatch(password, "[A-Z]"))
+					return false;
+
+				
+				if (!Regex.IsMatch(password, "[a-z]"))
+					return false;
+
+				
+				if (!Regex.IsMatch(password, "[0-9]"))
+					return false;
+
+				
+			}
+			return true;
+		}
+
+		public static string Capitalize(this string word)
+		{
+			return  word.Substring(0, 1).ToUpper() + word.Substring(1).ToLower();
+		}
+	}
+}
---
+++ StaticClass/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace StaticClass
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//int number = 5;
+			//         Console.WriteLine(number.IsOdd());
+			//         Console.WriteLine(number.IsEven());
+			//string word = "justincase";
+			//Console.WriteLine(word.HasDigit());
+
+			//string password = Console.ReadLine();
+			//         Console.WriteLine(password.CheckPassword());
+
+			string word = Console.ReadLine();
+            Console.WriteLine(word.Capitalize());
+		}
+	}
+}
